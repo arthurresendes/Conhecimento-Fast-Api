ec401ba4a4563253da182c4dd0d17542e033073b
Arthur Resende <arthur.resende.gomes02@gmail.com>
2026-04-18 15:14:46 -0300

    Relaçoes sqlalchemy

--- Aprendizado/SQLAlchemy/Aula/main.py
+++ Aprendizado/SQLAlchemy/Aula/main.py
@@ -1,5 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, sessionmaker, Relationship
 
 engine = create_engine('sqlite:///dados.db')
 Base = declarative_base()
@@ -11,6 +11,21 @@ class Usuario(Base):
     id: int = Column(Integer, autoincrement=True, primary_key=True)
     nome: str = Column(String(40), unique=True)
 
+class Livro(Base):
+    __tablename__ = 'livros'
+    
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    nome = Column(String(200))
+    ano = Column(Integer)
+    autor_id = Column(Integer, ForeignKey('autores.id'))
+    autor = Relationship('Autor', backref='livros', lazy='subquery')
+
+class Autor(Base):
+    __tablename__ = 'autores'
+    
+    id = Column(Integer, primary_key=True,autoincrement=True)
+    nome = Column(String(200))
+    idade = Column(Integer)
 
 # Base.metadata.create_all(engine) Depois de criado pode comentar embora ele ignore
 
@@ -46,3 +61,14 @@ with Sessao() as sessao:
     sessao.commit()
     sessao.close()
 
+
+with Sessao() as sessao:
+    livro = Livro(nome="Chapeuzinho Vermelho", ano=2026, autor_id=1)
+    sessao.add(livro)
+    sessao.commit()
+    sessao.close()
+
+with Sessao() as sessao:
+    autor = sessao.query(Autor).filter_by(id=1).first()
+    for livros in autor.livros:
+        print(livros.nome)
